36c42895b6b0a7c40f9b1305e57a2aacd685e1f9
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-12-21 05:45:35 +0800

    Refactored the system test

--- src/system-test/bitcoin-co-id.spec.ts
+++ src/system-test/bitcoin-co-id.spec.ts
@@ -2,15 +2,17 @@ import { expect } from "chai";
 import "mocha";
 import { isArray } from "util";
 import * as winston from "winston";
+import { BitcoinCoIdService } from "..";
 import {
-  BitcoinCoIdService,
   IGetOrderArgs,
   IOpenOrderArgs,
   IOrderHistoryArgs,
   ITradeHistoryArgs,
+} from "../lib/contracts/api-args";
+import {
   XRP_BTC,
   XRP_IDR,
-} from "../";
+} from "../lib/pair.constants";
 
 const BITCOINCOID_API_KEY = process.env.BCI_AK;
 const BITCOINCOID_SECRET_KEY = process.env.BCI_SK;
@@ -25,42 +27,49 @@ describe("BitcoinCoIdService", () => {
       new BitcoinCoIdService(BITCOINCOID_API_KEY, BITCOINCOID_SECRET_KEY);
   });
 
-  it("should give user's balances and server's timestamp", async () => {
+  it("should give user's balances and server's timestamp", () => {
     // Act
-    const response = await bitcoinCoIdService.getInfo();
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.success).to.be.equal(1);
-    expect(response.return.balance).to.be.not.equal(undefined);
-    expect(response.return.server_time).to.be.not.equal(0);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.getInfo();
+    }).then((response) => {
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.success).to.be.equal(1);
+      expect(response.return.balance).to.be.not.equal(undefined);
+      expect(response.return.server_time).to.be.not.equal(0);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
   });
 
-  it("get specific order details", async () => {
+  it("get specific order details", () => {
     // Arrange
-    const orderId: number = await getAnyOrderIdFromOrderHistory(XRP_IDR);
-    const getOrderArgs: IGetOrderArgs = {
-      order_id: orderId,
-      pair: XRP_IDR,
-    };
+    let orderId;
 
-    // Act
-    const response = await bitcoinCoIdService.getOrder(getOrderArgs);
+    return Promise.resolve().then(() => {
+      return getAnyOrderIdFromOrderHistory(XRP_IDR);
+    }).then( (getAnyOrderIdFromOrderHistoryResponse) => {
+      orderId = getAnyOrderIdFromOrderHistoryResponse;
+      const getOrderArgs: IGetOrderArgs = {
+        order_id: orderId,
+        pair: XRP_IDR,
+      };
 
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.success).to.be.equal(1);
-    expect(response.return.order.order_id).to.be.equal(orderId.toString());
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
+      // Act
+      return bitcoinCoIdService.getOrder(getOrderArgs);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.success).to.be.equal(1);
+      expect(response.return.order.order_id).to.be.equal(orderId.toString());
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
 
   });
 
-  it("should give gives the list of current open orders (buy and sell).", async () => {
+  it("should give gives the list of current open orders (buy and sell).", () => {
     // Arrange
     // TODO: Open an XRP Order 1st.
     const openOrderArgs: IOpenOrderArgs = {
@@ -68,21 +77,23 @@ describe("BitcoinCoIdService", () => {
     };
 
     // Act
-    const response = await bitcoinCoIdService.openOrders(openOrderArgs);
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.success).to.be.equal(1);
-    expect( isArray(response.return.orders)).to.be.equal(true);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.openOrders(openOrderArgs);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.success).to.be.equal(1);
+      expect( isArray(response.return.orders)).to.be.equal(true);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
 
     // Cleanup
     // TODO: Cancel the XRP Order.
   });
 
-  it("gives the list of order history (buy and sell).", async () => {
+  it("gives the list of order history (buy and sell).", () => {
     // Arrange
     // NOTE: Ensure that your bitcoin.co.id's account has confirmed one or more buy/sell order in past.
     const orderHistoryArgs: IOrderHistoryArgs = {
@@ -90,47 +101,50 @@ describe("BitcoinCoIdService", () => {
     };
 
     // Act
-    const response = await bitcoinCoIdService.orderHistory(orderHistoryArgs);
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.success).to.be.equal(1);
-    expect( isArray(response.return.orders) && response.return.orders.length > 0).to.be.equal(true);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
-
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.orderHistory(orderHistoryArgs);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.success).to.be.equal(1);
+      expect( isArray(response.return.orders) && response.return.orders.length > 0).to.be.equal(true);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
   });
 
-  it("gives list of deposits and withdrawals of all currencies.", async () => {
+  it("gives list of deposits and withdrawals of all currencies.", () => {
     // Act
-    const response = await bitcoinCoIdService.transHistory();
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.success).to.be.equal(1);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
-
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.transHistory();
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.success).to.be.equal(1);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
   });
 
-  it("gives information about transaction in buying and selling history.", async () => {
+  it("gives information about transaction in buying and selling history.", () => {
     // Arrange
     const tradeHistoryArgs: ITradeHistoryArgs = {
       pair: XRP_IDR,
     };
 
     // Act
-    const response = await bitcoinCoIdService.tradeHistory(tradeHistoryArgs);
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.success).to.be.equal(1);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
-
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.tradeHistory(tradeHistoryArgs);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.success).to.be.equal(1);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
   });
 
   async function getAnyOrderIdFromOrderHistory(pair: string): Promise<number> {
@@ -148,37 +162,41 @@ describe("BitcoinCoIdService", () => {
     return -1;
   }
 
-  it("gives ticker information of a cryptocurrency", async () => {
+  it("gives ticker information of a cryptocurrency", () => {
     // Arrange
     const pair = XRP_IDR;
 
     // Act
-    const response = await bitcoinCoIdService.ticker(pair);
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.ticker(pair);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(response.ticker).to.be.not.equal(undefined);
+      expect(response.ticker).to.be.not.equal(null);
+      expect(response.ticker.server_time).to.be.greaterThan(0);
 
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(response.ticker).to.be.not.equal(undefined);
-    expect(response.ticker).to.be.not.equal(null);
-    expect(response.ticker.server_time).to.be.greaterThan(0);
-
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
   });
 
-  it("gives trades information of a cryptocurrency", async () => {
+  it("gives trades information of a cryptocurrency", () => {
     // Arrange
     const pair = XRP_IDR;
 
     // Act
-    const response = await bitcoinCoIdService.trades(pair);
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(Array.isArray(response) && response.length > 0).to.be.equal(true);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response = \n", JSON.stringify(response, '', 2));
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.trades(pair);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(Array.isArray(response) && response.length > 0).to.be.equal(true);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response = \n", JSON.stringify(response, '', 2));
+    });
   });
 
   it("gives a list of trading depth info (buy/sell).", async () => {
@@ -186,18 +204,20 @@ describe("BitcoinCoIdService", () => {
     const pair = XRP_IDR;
 
     // Act
-    const response = await bitcoinCoIdService.depth(pair);
-
-    // Assert
-    expect(response).to.be.not.equal(undefined);
-    expect(response).to.be.not.equal(null);
-    expect(Array.isArray(response.buy) && response.buy.length > 0).to.be.equal(true);
-    expect(Array.isArray(response.sell) && response.sell.length > 0).to.be.equal(true);
-    // tslint:disable-next-line:quotemark
-    winston.debug("response.buy = \n", JSON.stringify(response.buy, "", 2));
-    // tslint:disable-next-line:quotemark
-    winston.debug("response.sell = \n", JSON.stringify(response.sell, "", 2));
-    winston.debug("response.buy.length = ", response.buy.length);
-    winston.debug("response.sell.length = ", response.sell.length);
+    return Promise.resolve().then(() => {
+      return bitcoinCoIdService.depth(pair);
+    }).then((response) => {
+      // Assert
+      expect(response).to.be.not.equal(undefined);
+      expect(response).to.be.not.equal(null);
+      expect(Array.isArray(response.buy) && response.buy.length > 0).to.be.equal(true);
+      expect(Array.isArray(response.sell) && response.sell.length > 0).to.be.equal(true);
+      // tslint:disable-next-line:quotemark
+      winston.debug("response.buy = \n", JSON.stringify(response.buy, "", 2));
+      // tslint:disable-next-line:quotemark
+      winston.debug("response.sell = \n", JSON.stringify(response.sell, "", 2));
+      winston.debug("response.buy.length = ", response.buy.length);
+      winston.debug("response.sell.length = ", response.sell.length);
+    });
   });
 });
